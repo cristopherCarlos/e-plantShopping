ee077ac38c5c2cbb64931dce04e9a451762afc1f
Cristopher Carlos Muñoz Avile <cristopher.munoz@COLUMBIA.CORE>
2026-03-31 18:07:27 -0500

    Tarea 6: Configuración de Provider y preparación para despliegue

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { Provider } from 'react-redux'
-import store from './store.js'
+import { Provider } from 'react-redux'; // Importamos el Provider
+import store from './store.js';       // Importamos el store que creamos
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <App />
     </Provider>
   </React.StrictMode>,
 )
--- src/store.js
+++ src/store.js
@@ -1,8 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './CartSlice';
- const store = configureStore({
+
+/**
+ * Tarea 5: Configuración de la Tienda Redux
+ * * Utilizamos configureStore para centralizar el estado de la aplicación.
+ * El 'cartReducer' manejará todas las acciones relacionadas con el carrito
+ * (agregar, eliminar, actualizar cantidad) definidas en CartSlice.jsx.
+ */
+const store = configureStore({
     reducer: {
+        // 'cart' es la clave que usaremos en useSelector(state => state.cart.items)
         cart: cartReducer,
     },
 });
-export default store
+
+// Exportación predeterminada para envolver la aplicación en el componente <Provider>
+export default store;
